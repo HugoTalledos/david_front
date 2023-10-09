e6b62e016f04020ed015c32624db0c105b3bc2da
HugoTalledos <hugo.talledos@bpsmart.ai>
2023-10-08 19:28:55 -0500

    Add menu options

---
+++ src/components/Card.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import {ReactComponent as MenuSVG} from '../assets/icons/More.svg';
+const Card = ({ setId, setName, songsConfig, setDescription, onDelete }) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex justify-end px-4 pt-4" style={{ position: 'relative' }}>
+        <button id="dropdownButton"
+                onClick={() => setOpen(!open)}
+                data-dropdown-toggle="dropdown"
+                className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
+          <span className="sr-only">Open dropdown</span>
+          <MenuSVG />
+        </button>
+        <div id="dropdown" style={{ position: 'absolute', inset: '0px auto auto 0px', margin: '0px', transform: 'translate(200px, 50px)' }}
+             className={`z-10 ${ open ? '' : 'hidden' } text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+          <ul className="py-2" aria-labelledby="dropdownButton">
+            <li>
+              <a href={`/edit/${setId}`}
+                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                Editar</a>
+            </li>
+            <li>
+              <a  href={`/view/${setId}`}
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                Ver</a>
+            </li>
+            <li>
+              <span onClick={onDelete} className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Eliminar</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex flex-col items-center px-4 pb-4">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{ setName }</h5>
+        <div className="flex mt-4 md:mt-6 pb-2">
+          <span className="text-md text-gray-500 dark:text-gray-400">Canciones</span>
+          <span className="ml-auto text-md text-gray-500 dark:text-gray-400">30 min</span>
+        </div>
+        <ul className="pl-2 pb-2 max-w-md space-y-1 text-sm text-gray-500 list-disc list-inside dark:text-gray-400">
+          { (songsConfig || []).map(({ songId, songName }) => (<li key={songId}>{songName}</li>)) }
+        </ul>
+        <h6 className="text-lg font-bold dark:text-white">Nota: </h6>
+        <p className="font-normal text-gray-700 dark:text-gray-400">{ setDescription }</p>
+        <div className="flex mt-4 space-x-3 md:mt-6">
+          <a href={`/view/${setId}`}
+             className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Ver Set</a>
+        </div>
+      </div>
+    </div>);
+}
+
+export default Card;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState, useContext } from 'react';
 import NotificationContext from '../context/notification-context';
+import Card from '../components/Card';
 
 import { getSets } from '../services/Set';
 const Home = () => {
@@ -12,6 +13,9 @@ const Home = () => {
     .catch(() => dispatchData({ type: 'danger', text: 'Ocurrio un error obteniendo los sets. Intenta mas tarde.' }));
   }, []);
 
+  const deleteSet = (setId) => {
+    console.log(setId)
+  }
   
   return (
     <main className="flex min-h-screen flex-col ml-64 p-10">
@@ -22,22 +26,7 @@ const Home = () => {
             <a href='/create' className="ml-auto py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Crear Set</a>    
           </div>
           <div className="flex mt-4 space-x-3 md:mt-6">
-            {
-              (sets || []).map(({ setId, setName, setDescription, songsConfig, ...all }) => (<>
-                <a href={`/view/${setId}`} key={setId} className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{ setName }</h5>
-                  <div className="flex mt-4 md:mt-6 pb-2">
-                    <span className="text-md text-gray-500 dark:text-gray-400">Canciones</span>
-                    <span className="ml-auto text-md text-gray-500 dark:text-gray-400">30 min</span>
-                  </div>
-                  <ul className="pl-2 pb-2 max-w-md space-y-1 text-sm text-gray-500 list-disc list-inside dark:text-gray-400">
-                    { (songsConfig || []).map(({ songId, songName }) => (<li key={songId}>{songName}</li>)) }
-                  </ul>
-                  <h6 className="text-lg font-bold dark:text-white">Nota: </h6>
-                  <p className="font-normal text-gray-700 dark:text-gray-400">{ setDescription }</p>
-                </a>
-              </>))
-            }
+            { (sets || []).map((props) => (<Card {...props} onDelete={() => deleteSet(props.setId)} />)) }
           </div>
         </div>
       </div>
