1948f62dbfd2787ddd52920837b0fd9e027ca924
HugoTalledos <hugo.talledos@bpsmart.ai>
2024-01-19 23:53:16 -0500

    TextView component added

---
+++ src/components/TextView.js
@@ -0,0 +1,29 @@
+import { markToHTML } from "../utils/Utils";
+
+const TextView = ({
+  songName = '',
+  singerName = '',
+  songTonality = '',
+  songTempo = '',
+  songLyric = '',
+  songChords = '',
+  onlyLyrics = false
+}) => (
+  <div className="border-2 border-gray-200 w-[60%] p-6 border-dashed rounded-lg dark:border-gray-700">
+    <div className='flex flex-col grow'>
+    <div className="mb-6">
+      <h5 class="text-xl font-bold dark:text-white">{ songName }</h5>
+      <h6 class="text-lg font-bold dark:text-white">{ singerName }</h6>
+      <div className="flex items-center justify-between mt-4">
+        <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Tonalidad: { songTonality }</p>
+        <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Tempo: { songTempo } BPM</p>
+      </div>
+    </div>
+    <div className="overflow-auto p-6 bg-white border border-gray-200 rounded-lg shadow h-[47vh]">
+      <p className="mb-3 text-gray-500 dark:text-gray-400"
+        dangerouslySetInnerHTML={{ __html: markToHTML(onlyLyrics ? songLyric : songChords) }}/>
+    </div>
+  </div>
+</div>);
+
+export default TextView;
