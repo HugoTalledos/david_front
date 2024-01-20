fdf153322669ccf6d19395c0bc8fb6b042d7d4d4
HugoTalledos <hugo.talledos@bpsmart.ai>
2024-01-20 01:06:49 -0500

    Change class to className

--- src/components/SongList/SongList.js
+++ src/components/SongList/SongList.js
@@ -3,11 +3,11 @@ const SongList = ({ list, onClickRow = undefined }) => (
   <div className="songList-layout border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-900 uppercase dark:text-gray-400">
+        <thead className="text-xs text-gray-900 uppercase dark:text-gray-400">
           <tr>
-            <th scope="col" class="px-6 py-3">Canción</th>
-            <th scope="col" class="px-6 py-3">Tono</th>
-            <th scope="col" class="px-6 py-3">BPM</th>
+            <th scope="col" className="px-6 py-3">Canción</th>
+            <th scope="col" className="px-6 py-3">Tono</th>
+            <th scope="col" className="px-6 py-3">BPM</th>
           </tr>
         </thead>
         <tbody>
--- src/components/TextView.js
+++ src/components/TextView.js
@@ -12,14 +12,14 @@ const TextView = ({
   <div className="border-2 border-gray-200 w-[60%] p-6 border-dashed rounded-lg dark:border-gray-700">
     <div className='flex flex-col grow'>
     <div className="mb-6">
-      <h5 class="text-xl font-bold dark:text-white">{ songName }</h5>
-      <h6 class="text-lg font-bold dark:text-white">{ singerName }</h6>
+      <h5 className="text-xl font-bold dark:text-white">{ songName }</h5>
+      <h6 className="text-lg font-bold dark:text-white">{ singerName }</h6>
       <div className="flex items-center justify-between mt-4">
         <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Tonalidad: { songTonality }</p>
         <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Tempo: { songTempo } BPM</p>
       </div>
     </div>
-    <div className="overflow-auto p-6 bg-white border border-gray-200 rounded-lg shadow h-[47vh]">
+    <div className="overflow-auto p-6 bg-white border border-gray-200 rounded-lg shadow h-[51vh]">
       <p className="mb-3 text-gray-500 dark:text-gray-400"
         dangerouslySetInnerHTML={{ __html: markToHTML(onlyLyrics ? songLyric : songChords) }}/>
     </div>
