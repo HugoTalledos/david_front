39985d6e83f415147691539543320d53b2abc851
HugoTalledos <hugo.talledos@bpsmart.ai>
2024-01-24 22:07:11 -0500

    Change tabindex for tabIndex

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,7 @@
 import { Button } from "leita-components-ui";
 
 const Modal = ({ isOpen, onClose, title, children }) => (<>
-  <div id="default-modal" tabindex="-1" aria-hidden="true" className={`${isOpen ? '' : 'hidden'} overflow-y-auto overflow-x-hidden fixed top-20 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
+  <div id="default-modal" tabIndex="-1" aria-hidden="true" className={`${isOpen ? '' : 'hidden'} overflow-y-auto overflow-x-hidden fixed top-20 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
     <div className="relative p-4 max-w-2xl max-h-full m-auto">
       <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
         <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
