fb2c82afacc98ca31b08974725db745c44ee642e
HugoTalledos <hugo.talledos@bpsmart.ai>
2024-01-25 21:15:54 -0500

    Change textview styles

--- src/components/TextView.js
+++ src/components/TextView.js
@@ -9,12 +9,12 @@ const TextView = ({
   songChords = '',
   onlyLyrics = false
 }) => (
-  <div className="box-border mb-5 max-[767px]:mb-2 w-[60%] max-[767px]:w-full max-[767px]:h-[70vh] p-3 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+  <div className="box-border max-[767px]:mb-2 w-[60%] max-[767px]:w-full max-[767px]:h-full p-3 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
     <div className='flex flex-col grow h-full'>
       <div className="mb-6  max-[767px]:mb-3">
-        <h5 className="text-xl font-bold dark:text-white  max-[767px]:hidden">{ songName }</h5>
-        <h6 className="text-lg font-bold dark:text-white  max-[767px]:text-base">{ singerName }</h6>
         <div className="flex items-center justify-between mt-4">
+          <h5 className="text-xl font-bold dark:text-white  max-[767px]:hidden">{ songName }</h5>
+          <h6 className="text-lg font-bold dark:text-white  max-[767px]:text-base">{ singerName }</h6>
           <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 max-[767px]:text-base">Tonalidad: { songTonality }</p>
           <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 max-[767px]:text-base">Tempo: { songTempo } BPM</p>
         </div>
